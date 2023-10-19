bb9b42e220ce0e2f318c6f8b54c31cb7b3f2af4c
BaThanh2303 <thanhnbth2210021@fpt.edu.vn>
2023-10-19 14:00:59 +0700

    commit

--- TranSportX/resources/views/auth/login.blade.php
+++ TranSportX/resources/views/auth/login.blade.php
@@ -1,47 +1,91 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@include("layouts.head")
+    <div id="main-wrapper" class="oxyy-login-register bg-dark">
+        <div class="container">
+            <div class="row g-0 min-vh-100 py-4 py-md-5">
+                <!-- Welcome Text
+                ========================= -->
+                <div class="col-lg-7 shadow-lg">
+                    <div class="hero-wrap d-flex align-items-center rounded-3 rounded-end-0 h-100">
+                        <div class="hero-mask opacity-9 bg-primary"></div>
+                        <div class="hero-bg hero-bg-scroll" style="background-image:url('https://www.easyhaul.com/blog/wp-content/uploads/2021/07/Blog-EasyHaul-Modes-of-Transport-Title-1-1000x563.png');"></div>
+                        <div class="hero-content mx-auto w-100 h-100 d-flex flex-column">
+                            <div class="row g-0">
+                                <div class="col-11 col-lg-10 mx-auto">
+                                    <div class="logo mt-5 mb-5 mb-lg-0"> <a href="https://harnishdesign.net/demo/html/oxyy/index.html" title="Oxyy"><img src="assets/img/logo/logo.png" alt="Oxyy"></a> </div>
+                                </div>
+                            </div>
+                            <div class="row g-0 my-auto">
+                                <div class="col-11 col-lg-10 mx-auto">
+                                    <h1 class="text-11 text-white mb-3">Welcome back!</h1>
+                                    <p class="text-5 text-white lh-base mb-4">Get access to your Orders, Wishlist and Recommendations.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Welcome Text End -->
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+                <!-- Register Form
+                ========================= -->
+                <div class="col-lg-5 shadow-lg d-flex align-items-center rounded-3 rounded-start-0 bg-dark">
+                    <div class="container my-auto py-5">
+                        <div class="row">
+                            <div class="col-11 col-lg-10 mx-auto">
+                                <h3 class="text-white text-center mb-4">Sign Up</h3>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label class="form-label text-light" for="emailAddress">Email Address</label>
+                                        <input type="email" class="form-control" id="emailAddress" name="email" value="{{ old('email') }}" required placeholder="Enter Email">
+                                        @error('email')
+                                        <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label text-light" for="loginPassword">Password</label>
+                                        <input type="password" class="form-control" id="loginPassword" name="password" required placeholder="Enter Password">
+                                        @error('password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="row mt-4">
+                                        <div class="col">
+                                            <div class="form-check text-2">
+                                                <input id="remember-me" name="remember" class="form-check-input" type="checkbox">
+                                                <label class="form-check-label text-light" for="remember-me">Remember Me</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm text-2 text-end">
+                                            @if (Route::has('password.request'))
+                                                <a href="{{ route('password.request') }}">Forgot Password ?</a>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="d-grid my-4">
+                                        <button class="btn btn-primary" type="submit">Sign In</button>
+                                    </div>
+                                </form>
+
+                                <div class="d-flex align-items-center my-3">
+                                    <hr class="flex-grow-1 bg-dark-4">
+                                    <span class="mx-2 text-2 text-muted">Or with Social Profile</span>
+                                    <hr class="flex-grow-1 bg-dark-4">
+                                </div>
+                                <div class="d-flex flex-column align-items-center mb-4">
+                                    <ul class="social-icons social-icons-circle">
+                                        <li class="social-icons-facebook"><a href="#" data-bs-toggle="tooltip" data-bs-original-title="Log In with Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li class="social-icons-twitter"><a href="#" data-bs-toggle="tooltip" data-bs-original-title="Log In with Twitter"><i class="fab fa-twitter"></i></a></li>
+                                        <li class="social-icons-google"><a href="#" data-bs-toggle="tooltip" data-bs-original-title="Log In with Google"><i class="fab fa-google"></i></a></li>
+                                        <li class="social-icons-linkedin"><a href="#" data-bs-toggle="tooltip" data-bs-original-title="Log In with Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+                                    </ul>
+                                </div>
+                                <p class="text-2 text-center text-light mb-0">Don't have an account? <a href="{{url('register')}}">Sign Up</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Register Form End -->
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
--- TranSportX/resources/views/auth/reset-password.blade.php
+++ TranSportX/resources/views/auth/reset-password.blade.php
@@ -1,39 +1,76 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@include("layouts.head")
+<div id="main-wrapper" class="oxyy-login-register bg-dark">
+    <div class="container">
+        <div class="row g-0 min-vh-100 py-4 py-md-5">
+            <!-- Welcome Text
+            ========================= -->
+            <div class="col-lg-7 shadow-lg">
+                <div class="hero-wrap d-flex align-items-center rounded-3 rounded-end-0 h-100">
+                    <div class="hero-mask opacity-9 bg-primary"></div>
+                    <div class="hero-bg hero-bg-scroll" style="background-image:url('https://www.easyhaul.com/blog/wp-content/uploads/2021/07/Blog-EasyHaul-Modes-of-Transport-Title-1-1000x563.png');"></div>
+                    <div class="hero-content mx-auto w-100 h-100 d-flex flex-column">
+                        <div class="row g-0">
+                            <div class="col-11 col-lg-10 mx-auto">
+                                <div class="logo mt-5 mb-5 mb-lg-0"> <a href="https://harnishdesign.net/demo/html/oxyy/index.html" title="Oxyy"><img src="https://harnishdesign.net/demo/html/oxyy/images/logo-light.png" alt="Oxyy"></a> </div>
+                            </div>
+                        </div>
+                        <div class="row g-0 my-auto">
+                            <div class="col-11 col-lg-10 mx-auto">
+                                <h1 class="text-11 text-white mb-3">We care about your account security.</h1>
+                                <p class="text-5 text-white lh-base mb-4">Get access to your Orders, Wishlist and Recommendations.</p>
+                                <a class="btn btn-dark rounded-pill shadow-none video-btn py-1 px-3 d-inline-flex align-items-center mb-5" href="#" data-src="https://www.youtube.com/embed/9-IEcZOa4rg" data-bs-toggle="modal" data-bs-target="#videoModal"><span class="me-2 text-7"><i class="fas fa-play-circle"></i></span>Watch demo</a> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Welcome Text End -->
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+            <!-- Register Form
+            ========================= -->
+            <div class="col-lg-5 shadow-lg d-flex align-items-center rounded-3 rounded-start-0 bg-dark">
+                <div class="container my-auto py-5">
+                    <div class="row">
+                        <div class="col-11 col-lg-10 mx-auto">
+                            <h3 class="text-white text-center mb-4">Reset Password</h3>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            <!-- Begin Form -->
+                            <form method="POST" action="{{ route('password.store') }}" class="form-dark">
+                                @csrf
+
+                                <!-- Password Reset Token -->
+                                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                <div class="mb-3">
+                                    <label class="form-label text-light" for="email">Email Address</label>
+                                    <input type="email" class="form-control" id="email" name="email" :value="old('email', $request->email)" required placeholder="Enter Your Email">
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label text-light" for="password">Password</label>
+                                    <input type="password" class="form-control" id="password" name="password" required placeholder="Enter Password">
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label text-light" for="password_confirmation">Confirm Password</label>
+                                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required placeholder="Confirm Password">
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-danger" />
+                                </div>
+
+                                <div class="d-grid my-4">
+                                    <button class="btn btn-primary" type="submit">Reset Password</button>
+                                </div>
+                            </form>
+
+
+
+                            <p class="text-2 text-center text-light">Already have an account? <a href="{{url('login')}}">Sign In</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Register Form End -->
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</div>
